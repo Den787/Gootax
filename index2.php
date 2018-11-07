8f59677fbb42d4ff6a5159322b90d293bcab7afa
Den787 <Den787@yandex.ru>
2018-11-07 16:18:11 +0400

    Ветвления и функции
    Рекурсия это полный ******

--- index2.php
+++ index2.php
@@ -16,7 +16,7 @@ const number2 = 10;
 $a = number1;
 $b = number2;
 
-   function doArithmeticOperations($a,$b)
+   function doOperations($a, $b)
    {
        if ($a >= 0 && $b >= 0) {
             return $a + $b;
@@ -33,15 +33,15 @@ $b = number2;
 
    }
 
-    print_r(doArithmeticOperations($a,$b));
+    print_r(doOperations($a,$b));
    echo '<br>';
-    print_r(doArithmeticOperations(-$a,$b));
+    print_r(doOperations(-$a,$b));
    echo '<br>';
-    print_r(doArithmeticOperations(-$a,-$b));
+    print_r(doOperations(-$a,-$b));
    echo '<br>';
 
 echo '<br/>';
-echo '-----------------------------------------------------';
+echo '<hr>';
 echo '<br/>';
 
 /*
@@ -50,7 +50,7 @@ echo '<br/>';
 
    print_r($a > $b ? $a : $b);
 echo '<br/>';
-echo '-----------------------------------------------------';
+echo '<hr>';
 echo '<br/>';
 
    /*
@@ -91,5 +91,142 @@ echo '<br/>';
    }
 
 echo '<br/>';
-echo '-----------------------------------------------------';
+echo '<hr>';
+echo '<br/>';
+
+/*
+ * 4. Реализуйте все арифметические операции в виде функций с двумя параметрами.
+*/
+function doSum($num1,$num2) {
+    return $num1 + $num2;
+}
+print_r(doSum(5,2));
+echo '<br/>';
+
+function doDiff($num1,$num2) {
+    return $num1 - $num2;
+}
+print_r(doDiff(5,2));
+echo '<br/>';
+
+function doDiv($num1,$num2) {
+    return $num1 / $num2;
+}
+print_r(doDiv(5,2));
+echo '<br/>';
+
+function doMult($num1,$num2) {
+    return $num1 * $num2;
+}
+print_r(doMult(5,2));
+
+
+echo '<br/>';
+echo '<hr>';
+echo '<br/>';
+
+/*
+ * 5. Реализуйте функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation), где $arg1,
+ * $arg2 – значения аргументов. $operation – строка с названием операции. В зависимости от переданного значения
+ *  операции выполните одну из арифметических операций (используйте функции из пункта 4) и верните полученное
+ * значение (используйте switch).
+ */
+
+function mathOperation($arg1,$arg2,$operation) {
+    switch ($operation) {
+        case '-' :
+            return doDiff($arg1,$arg2);
+            break;
+        case '+':
+            return doSum($arg1,$arg2);
+            break;
+        case '*':
+            return doMult($arg1,$arg2);
+            break;
+        case '/':
+            return doDiv($arg1,$arg2);
+            break;
+    }
+}
+
+
+print_r(mathOperation(5,2,'-'));
+echo '<br/>';
+print_r(mathOperation(5,2,'+'));
+echo '<br/>';
+print_r(mathOperation(5,2,'*'));
+echo '<br/>';
+print_r(mathOperation(5,2,'/'));
+
+echo '<br/>';
+echo '<hr>';
+echo '<br/>';
+
+/*
+ * 6. С помощью рекурсии организуйте функцию возведения числа в степень. Формат: function power($val, $pow),
+ * где $val – заданное число, $pow – степень
+ */
+function power($val,$pow) {
+if($pow === 0) {
+    return 1;
+}
+elseif ($pow > 0) {
+    return $val * power($val, $pow - 1); //2*2*2*2*1
+}
+elseif ($pow < 0) {
+    return power(1/$val, -$pow);   //математическая формула для отриц. степени
+}
+}
+print_r(power(2, 4));
+
+echo '<br/>';
+echo '<hr>';
+echo '<br/>';
+
+/*
+ * 7. Написать функцию, которая принимают в качестве аргументов два числа и вычисляет из них большее. Написать такую
+ *  же функцию, чтобы она вычисляла меньшее число. Проверить, если произведение $a и $b больше 100, но меньше 1000,
+ *  то от большего числа отнять меньшее и вывести результат на экран. А если произведение этих чисел больше 1000,
+ *  то произведение этих чисел разделить на большее из чисел.
+ */
+function highNumber($num1,$num2) {
+    if($num1 > $num2) {
+        return $num1;
+    }
+    elseif ($num1 < $num2) {
+        return $num2;
+    }
+    elseif ($num1 === $num2) {
+        return $num1;
+    }
+}
+print_r(highNumber(9,8));
+echo '<br/>';
+
+function smallNumber($num1,$num2) {
+    if($num1 > $num2) {
+        return $num2;
+    }
+    elseif ($num1 < $num2) {
+        return $num1;
+    }
+    elseif ($num1 === $num2) {
+        return $num1;
+    }
+}
+print_r(smallNumber(9,8));
+echo '<br/>';
+
+function doCheck($a,$b){
+    if(($a * $b > 100) && ($a * $b < 1000)) {
+       return highNumber($a,$b) - smallNumber($a,$b);
+    }
+    elseif ($a * $b > 1000) {
+        return ($a * $b) / highNumber($a,$b);
+    }
+}
+print_r(doCheck(100,15)); //5
+
+echo '<br/>';
+echo '<hr>';
 echo '<br/>';
