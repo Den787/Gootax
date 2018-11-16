806c598f848ebd44f0aa8fecc693c5c8b9be5d47
Den787 <Den787@yandex.ru>
2018-11-16 11:16:04 +0400

    готово

--- Calculator.php
+++ Calculator.php
@@ -1,8 +1,8 @@
 <?php
 include_once 'funcCalculator.php';
 
-
-$x = '';  //выводит пустое значение при первом открытии
+$x = '';
+$result = 0;
 
 if  (isset($_POST['submit'])) {
     $a = $_POST['a'];
@@ -10,27 +10,16 @@ if  (isset($_POST['submit'])) {
     $znak = $_POST['znak'];
 
 
-
-
-    if (!empty($a) && !empty($znak) && !empty($b)) {
-
-        if (is_numeric($a) && is_numeric($b)) {
+         if (is_numeric($a) && is_numeric($b)) {
 
             $result = calc($a, $znak, $b);
-
-            if ($result != false) {
-
-               print_r($result); //просто так для проверки
-               $x = $result;
-
-            }
-
+            $x = $result;
 
         }
-        else {
+         else {
+
             echo "Введены не числа";
 }
-    }
 
 }
 
@@ -40,21 +29,22 @@ if  (isset($_POST['submit'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Калькулятор</title>
+    <title>Title</title>
 </head>
 <body>
 
 <form action="Calculator.php" method="post">
     <p> Введите число 1: </p>
     <input type="text" name="a"><br>
-    <p> Введите знак: </p>
+    <p> Введите арифметическую операцию(+-*/): </p>
     <input type="text" name="znak"><br>
     <p> Введите число 2: </p>
     <input type="text" name="b"><br><br>
     <input type="submit" value="Посчитать" name="submit">
+    <input type="reset" value="Очистить форму">
     <p> Ответ: </p>
     <input type="text" disabled  value="<?php echo $x ?>">
-    <input type="reset" value="Очистить форму">
+
 </form>
 
 </body>
--- funcCalculator.php
+++ funcCalculator.php
@@ -3,31 +3,19 @@
         function calc($a,$znak,$b)
         {
 
-
-
             switch ($znak) {
-
                 case '-' :
                     $x = $a - $b;
                     return $x;
                 case '+' :
                     $x = $a + $b;
-
                     return $x;
                 case '*' :
                     $x = $a * $b;
-
                     return $x;
                 case '/' :
-                    if ($b != 0) {
-                        $x = $a / $b;
-                        return $x;
-                    }
-                    else {
-                        echo "На ноль делить нельзя";
-                        break;
-                    }
-
+                     $x = $a / $b;
+                     return $x;
 
                     }
             }
